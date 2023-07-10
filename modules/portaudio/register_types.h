471d8e81192c8894b153d1e2c45fe17a761915af
DeadlineEntertainment <118117411+DeadlineEntertainment@users.noreply.github.com>
2023-07-11 01:20:05 +0200

    again dont getting it

--- modules/portaudio/register_types.h
+++ modules/portaudio/register_types.h
@@ -1,13 +1,8 @@
-
 #ifndef PORTAUDIO_REGISTER_TYPES_H
 #define PORTAUDIO_REGISTER_TYPES_H
-
-#ifdef PORT_AUDIO
 #include "modules/register_module_types.h"
 
 void initialize_portaudio_module(ModuleInitializationLevel p_level);
 void uninitialize_portaudio_module(ModuleInitializationLevel p_level);
-
-#endif // PORT_AUDIO
 
 #endif // PORTAUDIO_REGISTER_TYPES_H
