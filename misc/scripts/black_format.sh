ad9238ac70578171da02d0c404cd5524ead6d2a0
DeadlineEntertainment <118117411+DeadlineEntertainment@users.noreply.github.com>
2023-07-10 21:55:19 +0200

    suck this!

--- misc/scripts/black_format.sh
+++ misc/scripts/black_format.sh
@@ -6,7 +6,7 @@ set -uo pipefail
 
 # Apply black.
 echo -e "Formatting Python files..."
-PY_FILES=$(git ls-files -- '*SConstruct' '*SCsub' '*.py' ':!:.git/*' ':!:thirdparty/*')
+PY_FILES=$(git ls-files -- '*SConstruct' '*SCsub' '*.py' ':!:.git/*' ':!:thirdparty/*' ':!:modules/portaudio*')
 black -l 120 $PY_FILES
 
 diff=$(git diff --color)
