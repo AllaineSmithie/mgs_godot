c6fbe0816301df323ebf1ff3ff21786912c12aa5
DeadlineEntertainment <118117411+DeadlineEntertainment@users.noreply.github.com>
2023-07-11 01:15:11 +0200

    wow

--- modules/portaudio/register_types.h
+++ modules/portaudio/register_types.h
@@ -1,5 +1,7 @@
+
 #ifndef PORTAUDIO_REGISTER_TYPES_H
 #define PORTAUDIO_REGISTER_TYPES_H
+
 #ifdef PORT_AUDIO
 #include "modules/register_module_types.h"
 
@@ -7,4 +9,5 @@ void initialize_portaudio_module(ModuleInitializationLevel p_level);
 void uninitialize_portaudio_module(ModuleInitializationLevel p_level);
 
 #endif // PORT_AUDIO
+
 #endif
